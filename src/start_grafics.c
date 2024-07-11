c8a550ac4ac7b687ca3f2b5c3cf65f1b99bccea6
albartol <albartol@student.42madrid.com>
2024-07-11 10:57:14 +0200

    minor change

--- src/start_grafics.c
+++ src/start_grafics.c
@@ -6,7 +6,7 @@
 /*   By: albartol <albartol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 14:44:53 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/07/10 16:21:35 by albartol         ###   ########.fr       */
+/*   Updated: 2024/07/10 22:05:14 by albartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ int start_grafics(t_game *data)
 	// mlx_set_setting(MLX_FULLSCREEN, true);
 	// data->display.mlx = mlx_init(data->display.width, data->display.height,
 	// 	"cube3d", false);
-	data->display.mlx = mlx_init(400, 400, "cube3d", false);
+	data->display.mlx = mlx_init(800, 600, "cube3d", false);
 	if (!data->display.mlx)
 		return (exit_msg(mlx_strerror(mlx_errno), EXIT_FAILURE));
 	mlx_loop(data->display.mlx);
+	mlx_terminate(data->display.mlx);
 	return (EXIT_SUCCESS);
 }
