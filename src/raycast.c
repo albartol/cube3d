6029aa3ad71ab1fedc1d4d8f579ddd4279c7529c
SherlockPE <fabriciolopezreyes@gmail.com>
2024-08-09 20:44:54 +0200

    ESTAN CERRANDO LA BIBLIOTECAAA AAAAAAAAAA
    COMMIT Y PUSH FAST
    
    CORRE FOREST CORRREEEE

--- src/raycast.c
+++ src/raycast.c
@@ -6,12 +6,38 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 09:28:31 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/08/09 19:34:43 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/08/09 20:42:21 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <raycast.h>
 #include <check_scene.h>
+
+
+uint32_t ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t t)
+{
+	return (r << 24 | g << 16 | b << 8 | t);
+}
+
+int	fill_screen(mlx_image_t *img, uint32_t start, double line_height, int side)
+{
+	static	uint32_t	j;
+	uint32_t			color;
+	if (side == 0)
+		color = ft_pixel(0, 255, 0, 255);
+	else
+		color = ft_pixel(0, 0, 255, 255);
+	if (j >= img->width)
+		return (EXIT_FAILURE);
+	while (start < line_height)
+	{
+		mlx_put_pixel(img, j, start, color);
+		start++;
+	}
+	j++;
+	return (EXIT_SUCCESS);
+}
+
 
 /* 
 Setea las cordenadas iniciales del player
@@ -52,6 +78,8 @@ int	raycast(t_game *data)
 	ray_data.camera_plane.x = 0;
 	ray_data.camera_plane.y = 0.66;
 
+	data->display.mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "Cube 3D", false);
+	data->display.frames[0] = mlx_new_image(data->display.mlx, WIN_WIDTH, WIN_HEIGHT);
 	x = 0;
 	while (x < WIN_WIDTH)
 	{
@@ -64,7 +92,7 @@ int	raycast(t_game *data)
 		// DDA
 		printf("\n_________Ray #%d_(%f)_________\n", x + 1, ray_data.camera_x);
 		line_height = dda(&ray_data, &dda_data, data->file.map);
-		printf("Linea a dibujar --> [%f]\n", line_height);
+		printf("Linea a dibujar --> [%f] hitted by (%d)\n", line_height, dda_data.side);
 		printf("\n__________________________\n");
 		x++;
 	}
