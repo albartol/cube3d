4f62b5893dcadfb6d28493afaba51d0836608754
Sherlock PE <flopez-r@student.42madrid.com>
2024-08-28 18:47:30 +0200

    Estaran cerrando los edificios en 15 mingit statusgit status
    dont panic and git commit && git push

--- src/hooks/hooks.c
+++ src/hooks/hooks.c
@@ -6,11 +6,25 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 12:11:53 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/08/28 14:36:57 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/08/28 18:46:35 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <raycast.h>
+
+int	mouse_hook(t_game *data)
+{
+	int32_t	x;
+	int32_t	y;
+	int		draw;
+
+	mlx_get_mouse_pos(data->display.mlx, &x, &y);
+	if (x < WIN_WIDTH / 2)
+		rotate_LR(&data->player, ROTATION_SPEED_R, 1.0, &draw)
+	mlx_set_mouse_pos(data->display.mlx, WIN_WIDTH / 2, WIN_HEIGHT / 2);
+	printf("Posicion del mouse en (%d, %d)\n", x, y);
+	return (0);
+}
 
 void	hooks(void *param)
 {
@@ -18,5 +32,7 @@ void	hooks(void *param)
 
 	data = (t_game *)param;
 	if (keys_hook(data))
+		draw_all(data);
+	if (mouse_hook(data))
 		draw_all(data);
 }
